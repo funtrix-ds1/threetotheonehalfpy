77fc3d37bd9e03933c2e9c530c1386706e6d528b
funtrix-ds1 <ft.ds.37382@gmail.com>
2023-07-18 11:04:23 +0300

    updated setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     author = 'funtrix_doesstuff',
     description = 'A Python package for rapid and secure calculation of three to the one half in an industrial environment',
     url = 'https://github.com/funtrix-ds1/threetotheonehalfpy',
-    long_description = long_desc
+    long_description = long_desc,
     long_description_content_type = text/markdown
     )

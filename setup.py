ebdb30f10968f7ab7cf4a67e20a2f8fa2826c53c
funtrix-ds1 <ft.ds.37382@gmail.com>
2023-07-18 10:58:26 +0300

    added project description

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'threetotheonehalfpy',
-    version = '1.0.0',
+    version = '1.0.1',
     author = 'funtrix_doesstuff',
     description = 'A Python package for rapid and secure calculation of three to the one half in an industrial environment',
     url = 'https://github.com/funtrix-ds1/threetotheonehalfpy'
